688adaa561fad2849ca4910dd44ff4df98b1db39
Mathieu Ledru <matyo91@darkwood.fr>
2021-12-26 18:20:30 +0100

    :white_check_mark: tests

---
+++ tests/GameTest.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+
+final class GameTest extends TestCase
+{
+    public function testValid(): void
+    {
+        $this->assertTrue(true);
+    }
+}
