af1cebcee3e9fd9d2f2c05e904ac7dd60dc21971
Mathieu Ledru <matyo91@darkwood.fr>
2022-01-07 17:49:23 +0100

    :white_check_mark: test coords

--- tests/GameTest.php
+++ tests/GameTest.php
@@ -1,10 +1,28 @@
 <?php declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
+use Warship\Game;
 
 final class GameTest extends TestCase
 {
-    public function testValid(): void
+    /**
+     * @dataProvider coordsProvider
+     */
+    public function testCoords($x, $y, $coord): void
     {
-        $this->assertTrue(true);
+        $game = new Game();
+        $gameCoord = $game->getCoord($x, $y);
+        $this->assertEquals($coord, $gameCoord);
+    }
+
+    public function coordsProvider()
+    {
+        return [
+            [0, 0, 'A1'],
+            [0, 4, 'A5'],
+            [3, 5, 'D6'],
+            [4, 8, 'E9'],
+            [5, 9, 'F10'],
+            [9, 0, 'J1'],
+        ];
     }
 }
